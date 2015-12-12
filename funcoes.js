b4d9bc93a82eca79c0a368deece8669dbd085814
demonkart <alanpcsilva@gmail.com>
2015-12-12 05:32:51 +0000

    Improve output
    Signed-off-by: demonkart <alanpcsilva@gmail.com>

--- funcoes.js
+++ funcoes.js
@@ -23,13 +23,12 @@ var img = new Image();
 img.src='car.png';
 
 function drawMap() {
-	for (var i=1;i<mapPoints.length;i=i+2){
-		p1=mapPoints[i-1];
-		p2=mapPoints[i];
+	for (var i=0;i<mapPoints.length;i=i+2){
+		p1=mapPoints[i];
+		p2=mapPoints[i+1];
 		context.beginPath();
 		context.moveTo(p1.x, p1.y);
 		context.lineTo(p2.x, p2.y);
-		console.log(p2.t+" | "+tempo);
 		context.strokeStyle = 'black';
 		context.stroke();
 		context.closePath();
@@ -40,6 +39,7 @@ function drawSplines (tempo) {
 	for (var i=1;i<points.length;i++){
 		p1=points[i-1];
 		p2=points[i];
+		
 		context.beginPath();
 		context.moveTo(p1.x, p1.y);
 		context.lineTo(p2.x, p2.y);
@@ -73,12 +73,29 @@ function rotateAndPaintImage ( context, image, angleInRad , positionX, positionY
   context.translate( -positionX, -positionY );
 }
 
+function prettyPrint(text,max,value) {
+	var blocks='';	
+	for (var i = 0; i < max; i++) {
+		blocks+='<div class="block"></div>';
+	}
+	return '<div class="title">'+text+' &#x2192; '+value+'</div>'+blocks+'<br>';
+}
+
 function drawAnimation(tempo) {
 	context.clearRect(0, 0, WIDTH, HEIGHT);
 	var p= points[tempo];
 	var TO_RADIANS = Math.PI/180;
 	drawMap();
 	drawSplines(p.t);
+	printX=prettyPrint('X',p.x/50, p.x);
+	printY=prettyPrint('Y',p.y/50, p.y);
+	printT=prettyPrint('Tempo',p.t/50, p.t/50);
+	printV=prettyPrint('Velocidade',p.v.toFixed(4)*10,p.v.toFixed(4));
+	var ang = (p.a+360)%360;
+	printA='<div class="title">Ângulo &#x2192; '+ang+
+	'</div><div class="block arrow" style="transform:rotate('+ang+'deg);">&#x21a3;</div><br>';
+	
+	document.getElementById('teste').innerHTML=printX+printY+printT+printV+printA;
 	// rotate 45º image "imgSprite", based on its rotation axis located at x=20,y=30 and draw it on context "ctx" of the canvas on coordinates x=200,y=100
 	rotateAndPaintImage ( context, img, p.a*TO_RADIANS, p.x, p.y, 46, 22 );
 	
@@ -131,7 +148,7 @@ function Atualizar() {
 	tempo=drawAnimation(tempo)%maxTempo;
 }
 
-window.addEventListener('mousemove', function (evt){document.getElementById('teste').innerHTML='X='+evt.clientX+'<br>Y='+evt.clientY;}, true);
+//window.addEventListener('mousemove', function (evt){document.getElementById('teste').innerHTML='X='+evt.clientX+'<br>Y='+evt.clientY;}, true);
 
 
 window.onload = function () {
